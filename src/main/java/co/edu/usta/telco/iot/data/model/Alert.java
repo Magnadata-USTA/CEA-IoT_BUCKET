b4e21c540e4484b7a6f02e90ebdbf80e343434be
Carlos <cjangaritas@gmail.com>
2017-04-12 13:43:56 -0500

    Adding alert properties/getters/setters

--- src/main/java/co/edu/usta/telco/iot/data/model/Alert.java
+++ src/main/java/co/edu/usta/telco/iot/data/model/Alert.java
@@ -20,4 +20,46 @@ public class Alert implements Serializable {
 
     private AlertType alertType;
 
+    private String sensorId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public AlertType getAlertType() {
+        return alertType;
+    }
+
+    public void setAlertType(AlertType alertType) {
+        this.alertType = alertType;
+    }
+
+    public String getSensorId() {
+        return sensorId;
+    }
+
+    public void setSensorId(String sensorId) {
+        this.sensorId = sensorId;
+    }
+
 }
