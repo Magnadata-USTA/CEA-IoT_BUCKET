89ae758efa378974c31c77e2a25de065ae4ce61d
Carlos <cjangaritas@gmail.com>
2017-04-19 16:32:52 -0500

    - adding mailFrom property

--- src/main/java/co/edu/usta/telco/iot/config/MailSenderImpl.java
+++ src/main/java/co/edu/usta/telco/iot/config/MailSenderImpl.java
@@ -2,6 +2,7 @@ package co.edu.usta.telco.iot.config;
 
 import co.edu.usta.telco.iot.exception.BusinessException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -20,14 +21,20 @@ public class MailSenderImpl{
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Value("${mail.repository.mailFrom}")
+    private String mailFrom;
+
+    @Value("${mail.repository.replyTo}")
+    private String replyTo;
+
     @Async
     public void send(String mailTo, String subject, String body) throws BusinessException{
         MimeMessage mail = javaMailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(mail, true);
             helper.setTo(mailTo);
-            helper.setReplyTo("cjangaritas@gmail.com");
-            helper.setFrom("cjangaritas@gmail.com");
+            helper.setReplyTo(replyTo);
+            helper.setFrom(mailFrom);
             helper.setSubject(subject);
             helper.setText(body);
             javaMailSender.send(mail);
