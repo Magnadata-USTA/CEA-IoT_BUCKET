de6e159b17fab27d4e85db69362081d4fca8ce27
Carlos <cjangaritas@gmail.com>
2017-04-21 18:06:42 -0500

    - Adding admin restrictions

--- src/main/java/co/edu/usta/telco/iot/config/WebSecurityConfig.java
+++ src/main/java/co/edu/usta/telco/iot/config/WebSecurityConfig.java
@@ -24,22 +24,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
-
-    @Override
-    public void configure(WebSecurity webSecurity) throws Exception
-    {
-        webSecurity
-                .ignoring()
-                // All of Spring Security will ignore the requests
-                .antMatchers("/lib/**", "/img/**", "/admin/**", "/api/**");
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/register/**", "/lib/**", "/admin/**").permitAll()
-//                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/", "/register/**", "/lib/**", "/img/**", "/api/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
